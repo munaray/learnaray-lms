ee12b84131176d293605831cde7f2a85cc386c47
munaray <munahdesigns@gmail.com>
2024-09-19 22:14:01 +0100

    Implementing Notification Schema

---
+++ src/schemas/notification.schema.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import { NotificationOptions } from "../@types/types.user";
+const notificationSchema = new Schema<NotificationOptions>(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			required: true,
+			default: "unread",
+		},
+	},
+	{ timestamps: true }
+);
+
+const Notification: Model<NotificationOptions> = mongoose.model(
+	"Notification",
+	notificationSchema
+);
+
+export default Notification;
