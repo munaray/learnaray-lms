fd18101f8e10305ad21b47211b46657fd8bd323d
munaray <saabdulsalam1215225@gmail.com>
2024-10-08 10:45:44 +0100

    Implementing rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,24 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
-import { middlewareErrorHandler } from "./middleware/error";
-import routes from "./routes/index";
-
-import swaggerUi from "swagger-ui-express";
-import OPENAPI_DOCS from "./swagger-docs/swagger";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+import rateLimit from "express-rate-limit";
+import compression from "compression";
+
+import routes from "./routes/index";
+import { middlewareErrorHandler } from "./middleware/error";
+import OPENAPI_DOCS from "./swagger-docs/swagger";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
 
 export const app = express();
+
+// Rate limiter
+app.use(limiter);
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -19,6 +29,9 @@ app.use(cookieParser());
 
 // cors => cross origin resource sharing
 app.use(cors());
+
+// compression
+app.use(compression());
 
 app.use(
   "/api-docs/swagger-ui",
